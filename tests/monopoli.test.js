b93ba7f421dece70b775263b989c07232a96fb65
David <dav.baz@alice.it>
2017-09-07 16:07:06 +0200

    Update monopoli.test.js

--- tests/monopoli.test.js
+++ tests/monopoli.test.js
@@ -3,8 +3,11 @@ const chai = require('chai');
 const assertArrays = require('chai-arrays');
 chai.use(assertArrays);
 const TestMonopoli = require('../src/monopoli');
+
+
+
 describe('TestMonopoli', () => {
-//output 20 turni
+
 it('roundTest should returns true if all rounds dont contain an error', () => { 
   
   const initialPosition = 0;
@@ -12,14 +15,14 @@ it('roundTest should returns true if all rounds dont contain an error', () => {
   const anna =  { name : "Anna" , index : 1 , position : initialPosition, balance : 1000, lastRoll : 0 ,predentCell:0};
   const carlo =  { name : "Carlo" , index : 2 , position : initialPosition, balance : 1000,lastRoll : 0 ,predentCell:0};
   
-  let players = TestMonopoli.randomlyStart([mario,anna,carlo]); //ordina i giocatori casualmente
+  let players = TestMonopoli.randomlyStart([mario,anna,carlo]);
 
   let rounds = [] ;
   console.log('PLAYERS');
   console.log(players);
   console.log('GAME');
   
-  for( let i = 0 ; i < 20 ; i++){ // simula 20 turni
+  for( let i = 0 ; i < 40 ; i++){// simula 20 turni
     for( let j = 0 ; j <= players.length ; j++){
       if( i % players.length === j ){
         rounds.push(TestMonopoli.round(players[j],0));
@@ -35,24 +38,23 @@ it('roundTest should returns true if all rounds dont contain an error', () => {
   }
   
 });
-//first relase
+
 it('doubleRollTest should returns true if result is an object', () => { 
   const result = TestMonopoli.doubleRoll();
   expect(result).to.be.a('object');
   
 });
 
-
-
 it('RandomlyStart should returns true if the array^s length is 5', () => { 
   console.log("first relase");
-  const poolPLayers = [{name : "Mario" , position : 0,index:1},{name : "Aldo" , position : 0,index:0},{name : "Anna" , position : 0,index:2},{name : "Clara" , position : 0,index:3},{name : "Ugo" , position : 0,index:4}];
+  const poolPLayers = [{name : "Mario" , position : 0,index:1,balance:0},{name : "Aldo" , position : 0,index:0,balance:0},{name : "Anna" , position : 0,index:2,balance:0},{name : "Clara" , position : 0,index:3,balance:0},{name : "Ugo" , position : 0,index:4,balance:0}];
   const result = TestMonopoli.randomlyStart(poolPLayers);
   
   expect(result).to.be.ofSize(5);
   
   
 });
+
 it('RandomlyStartTest should returns true if the error is "Parameter not valid"', () => { 
   let poolPLayers = 
   [
@@ -66,6 +68,7 @@ it('RandomlyStartTest should returns true if the error is "Parameter not valid"'
  
   
 });
+
 it('RandomlyStartTest should returns true if the error is "Parameter not valid"', () => { 
   const poolPLayers = [];
   const result = TestMonopoli.randomlyStart(poolPLayers);
@@ -163,14 +166,14 @@ it('roundTest should returns true if all rounds dont contain an error', () => {
 
 it('isGoTest should returns true if a player pass or lands from "GO" cell', () => { 
   console.log('second relase')
-    const mario = { name : "Mario" , index : 0 , position : 35};
+    const mario = { name : "Mario" , index : 0 , position : 35 , balance:0};
     const result = TestMonopoli.isGo(35 , 1);
     expect(result).equal( true );
   
 });
 
 it('isGoTest should returns true if a player does not pass from "GO" cell', () => { 
-  const mario = { name : "Mario" , index : 0 , position : 35};
+  const mario = { name : "Mario" , index : 0 , position : 35 , balance:0};
   const result = TestMonopoli.isGo(20 ,30);
   expect(result).equal( false );
 
@@ -220,14 +223,35 @@ it('isEqualDiceTest should returns true if a player rolls equal dice^s value', (
     
  
 });
-it('updatePlayerTest should returns true if result is an object', () => { 
+
+it('isEqualDiceTest should returns true if a player rolls not equal dice^s value', () => { 
   
-  const result = TestMonopoli.updatePlayer(7,{ name : "Mario" , index : 0 , position : 7 , balance : 1300});
-  expect(result).to.be.a('object');
-    
+  const result = TestMonopoli.isEqualDice(1,3);
+  expect(result).equal(false);    
  
 });
 
-
+it('isEqualDiceTest should returns true if returns an error', () => { 
+  const result = TestMonopoli.isEqualDice('a',3);
+  expect(result).to.have.property( 'error', 'Parameter not valid' );     
 });
+
+it('updatePlayerTest should returns true if result is an object', () => { 
+  const result = TestMonopoli.updatePlayer(7,{ name : "Mario" , index : 0 , position : 7 , balance : 1300});
+  expect(result).to.be.a('object');  
+ 
+});
+it('updatePlayerTest should returns true if returns an error', () => { 
+  const result = TestMonopoli.updatePlayer("a",{ name : "Mario" , index : 0 , position : 7 , balance : 1300});
+  expect(result).to.have.property( 'error', 'Parameter not valid' );  
+ 
+});
+
+it('isValidPLayerTest should returns true if player is correct', () => { 
+  const result = TestMonopoli.isValidPLayer({ name : "Mario" , index : 0 , position : 7 , balance : 1300});
+  expect(result).equal(true);  
+ 
+});
+
+})
 
